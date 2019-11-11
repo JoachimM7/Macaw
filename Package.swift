c218b319d6f00cee4c92f401093ade926ef20929
JoachimM7 <32777429+JoachimM7@users.noreply.github.com>
2019-11-11 15:20:48 +0100

    Update Package.swift
    Fix dependency 2

--- Package.swift
+++ Package.swift
@@ -3,16 +3,16 @@
 import PackageDescription
 
 let package = Package(
-	name: "Macaw",
-	dependencies: [
-		.package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "5.0.0")
-	],	
+	name: "Macaw",	
 	products: [
 		// Products define the executables and libraries produced by a package, and make them visible to other packages.
 		.library(
 			name: "Macaw",
 			targets: ["Macaw"]),
 	],
+	dependencies: [
+		.package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "5.0.0")
+	],	
 	targets: [
 		// Targets are the basic building blocks of a package. A target can define a module or a test suite.
 		// Targets can depend on other targets in this package, and on products in packages which this package depends on.
